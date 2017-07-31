785aa9c9202a0a83ec93e36f2e8986a3d72ce076
reflexdemon <venkateswara.vp@gmail.com>
2017-07-31 08:00:18 -0400

    fixing issue #2 with this commit

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,7 +30,7 @@ module.exports = class extends Generator {
     this.fs.copy(this.templatePath('package.json'), this.destinationPath('package.json'));
     this.fs.copy(this.templatePath('webpack.prod.js'), this.destinationPath('webpack.prod.js'));
     this.fs.copy(this.templatePath('webpack.config.js'), this.destinationPath('webpack.config.js'));
-    this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
+    this.fs.copy(this.templatePath('_.gitignore'), this.destinationPath('.gitignore'));
     this.fs.copy(this.templatePath('webpack.dev.js'), this.destinationPath('webpack.dev.js'));
     this.fs.copy(this.templatePath('yarn.lock'), this.destinationPath('yarn.lock'));
     this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
